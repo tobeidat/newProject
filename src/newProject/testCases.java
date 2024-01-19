081c9ca1d9cdbe1f784df6ea3438854ea5f30ebc
tobeidat <toqaobeidat1@gmail.com>
2024-01-20 00:22:11 +0300

    newproject

--- src/newProject/testCases.java
+++ src/newProject/testCases.java
@@ -1,5 +1,6 @@
 package newProject;
 
+import java.time.Duration;
 import java.util.List;
 import java.util.Random;
 import org.openqa.selenium.By;
@@ -25,30 +26,52 @@ public class testCases {
 
 	@Test()
 	public void myTest() throws InterruptedException {
-		Thread.sleep(2000);
-		WebElement chaltesElement = driver.findElement(By.cssSelector("#uncontrolled-tab-example-tab-chalets"));
+		WebElement chaltesElement = driver.findElement(By.id("uncontrolled-tab-example-tab-chalets"));
 		chaltesElement.click();
-		WebElement citiesList = driver.findElement(By.className("sc-gYtlsd"));
-		citiesList.click();
+		WebElement placeholderElement = driver.findElement(By.className("erpSii"));
+		placeholderElement.click();
+		Thread.sleep(2000);
 		WebElement theCity = driver.findElement(By.cssSelector("button:nth-child(7)"));
 		theCity.click();
-		WebElement searchButton = driver
-				.findElement(By.cssSelector(".Button__Btn-sc-xr32du-0.bwtwxz.sc-imAxmJ.gVJtmY"));
-		searchButton.click();
-		Thread.sleep(4000);
-		List<WebElement> chaltes = driver.findElements(By.className("Button__Btn-sc-xr32du-0"));
-		chaltes.get(1).click();
-		Thread.sleep(3000);
 		WebElement loginDateElement = driver.findElement(By.id("js-C2CGuestSearchBoxDatePickerCheckInDate"));
 		loginDateElement.click();
 
-		WebElement loginDate = driver.findElement(By.className("DayPicker-Day--today"));
-		loginDate.click();
-		WebElement logoutDate = driver.findElement(By.xpath(
-				"/html/body/div[2]/div/div[2]/div[2]/div[2]/div[1]/div/div[2]/div/div/div/div[2]/div[1]/div[2]/div[3]/div[7]"));
-		logoutDate.click();
+		List<WebElement> allDates = driver.findElements(By.className("DayPicker-Day"));
+		for (int i = 0; i < allDates.size(); i++) {
+			int num = rand.nextInt(allDates.size());
+			String isDateVisable = allDates.get(num).getAttribute("aria-disabled");
+			boolean isDateEnableAsBollen = Boolean.parseBoolean(isDateVisable);
+
+			if (isDateEnableAsBollen == true) {
+				continue;
+
+			}
+
+			allDates.get(num).click();
+			break;
+
+		}
 		Thread.sleep(2000);
-		WebElement bookNowButton = driver.findElement(By.className("btn-large"));
+		for (int j = 0; j < allDates.size(); j++) {
+			int number = rand.nextInt(allDates.size());
+			String isDateEnable = allDates.get(number).getAttribute("aria-disabled");
+			boolean isDateEnableAsBollen = Boolean.parseBoolean(isDateEnable);
+			if (isDateEnableAsBollen == true) {
+				continue;
+
+			}
+			allDates.get(number).click();
+			break;
+
+		}
+		WebElement searchButton = driver
+				.findElement(By.cssSelector(".Button__Btn-sc-xr32du-0.bwtwxz.sc-imAxmJ.gVJtmY"));
+		searchButton.click();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30000));
+		List<WebElement> chaltes = driver.findElements(By.className("btn-medium"));
+		chaltes.get(1).click();
+
+		WebElement bookNowButton = driver.findElement(By.className("BookNowButton__StyledButton-sc-1hapap0-0"));
 		bookNowButton.click();
 
 	}
